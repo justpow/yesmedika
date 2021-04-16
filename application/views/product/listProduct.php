eea7c4b14af849701ed11a431548f130bb9b1ee6
unknown <wunselan@gmail.com>
2021-04-16 20:20:09 +0700

    [Update] View daftar produk dan header

---
+++ application/views/product/listProduct.php
@@ -0,0 +1,149 @@
+
+<section class="head-home">
+    <div id="head-home" class="position-relative overflow-hidden p-3 p-md-5 text-center bg-dark text-light">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="display-4 fw-normal">YESMedika Alkes</h1>
+            <p class="lead fw-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cupiditate earum soluta culpa alias iure labore dicta. Velit soluta, possimus ea eos earum exercitationem libero. Ex quae incidunt eos optio.</p>
+            <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+        </div>
+    </div>
+</section>
+<section class="main-menu">
+    <div class="container my-5">
+        <div class="row">
+            <div class="card-filter col-md-12 col-lg-3">
+                <h6 class="fw-bold my-3">FILTER</h6>
+                <div class="card shadow" >
+                    <ul class="list-group">
+                        <li class="list-group-item" type="button" data-bs-toggle="collapse" data-bs-target="#ratingCollapse">Rating</li>
+                        <div class="collapse show multi-collapse" id="ratingCollapse">
+                            <div class="card card-body">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="5up">
+                                    <label class="form-check-label" for="5up">
+                                        <span class="fas fa-star text-warning"></span> 5 Ke Atas
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="4up">
+                                    <label class="form-check-label" for="4up">
+                                        <span class="fas fa-star text-warning"></span> 4 Ke Atas
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="3up">
+                                    <label class="form-check-label" for="3up">
+                                        <span class="fas fa-star text-warning"></span> 3 Ke Atas
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="2up">
+                                    <label class="form-check-label" for="2up">
+                                        <span class="fas fa-star text-warning"></span> 2 Ke Atas
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="1up">
+                                    <label class="form-check-label" for="1up">
+                                        <span class="fas fa-star text-warning"></span> 1 Ke Atas
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <li class="list-group-item" type="button" data-bs-toggle="collapse" data-bs-target="#hargaCollapse">Harga</li>
+                        <div class="collapse show multi-collapse" id="hargaCollapse">
+                            <div class="card card-body">
+                                <label for="harga-minimum" class="form-label">Harga Minimum</label>
+                                <div class="input-group input-group-sm mb-3">
+                                    <span class="input-group-text">Rp.</span>
+                                    <input type="number" class="form-control" placeholder="Harga Minimum">
+                                </div>
+                                <label for="harga-maksimum" class="form-label">Harga Maksimum</label>
+                                <div class="input-group input-group-sm mb-3">
+                                    <span class="input-group-text">Rp.</span>
+                                    <input type="number" class="form-control" placeholder="Harga Maksimum">
+                                </div>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+            <div class="card-product col-md-12 col-lg-9">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-xs-12 mb-3 mt-5">
+                        <div class="card shadow-sm text-center">
+                            <img src="<?= base_url('assets/image/perban.jpg') ?>" class="card-img-top" alt="product">
+                            <div class="card-body">
+                                <h5 class="card-title">Perban YESMedika</h5>
+                                <p class="card-text">Rp. 50.000/Box</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- application/views/template/head/head.php
+++ application/views/template/head/head.php
@@ -6,7 +6,7 @@
             <head>
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link href="'.base_url('assets/font-awesome-5.15.2/all.min.css').'" rel="stylesheet">
+                <link href="'.base_url('assets/font-awesome-5.15.3/css/all.min.css').'" rel="stylesheet">
                 <link href="'.base_url('assets/bootstrap-5.0.0/css/bootstrap.min.css').'" rel="stylesheet">
                 <link rel="stylesheet" href="'.base_url('assets/bootstrap-5.0.0/css/bootstrap-icons.css').'">
                 <link rel="stylesheet" href="'.base_url('assets/style.css').'">
